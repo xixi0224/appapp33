c2c3aa839366c6fc42d357360830f25785b49187
xixi0224 <xixi0224@github.com>
2026-07-20 09:57:14 +0800

    Fix Git conflicts and add auto-login after registration

--- zhinote-web/vite.config.js
+++ zhinote-web/vite.config.js
@@ -3,10 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-<<<<<<< HEAD
-=======
   base: '/',
->>>>>>> 4f174552fdd0bf3d635780d8f0719457d5ed4a57
   server: {
     proxy: {
       '/api': {
